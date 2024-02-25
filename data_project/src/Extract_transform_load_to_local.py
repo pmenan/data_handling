9ff209df9ec035ef77d8cecff3abcbc0ba5f4402
ndri_menan <menanpatrick49@gmail.com>
2024-02-25 18:28:06 +0100

    Application d'extraction, de transformation et chargement des données en local

--- data_project/src/Extract_transform_load_to_local.py
+++ data_project/src/Extract_transform_load_to_local.py
@@ -2,12 +2,30 @@
 
 # import des modules 
 from utils.elt_functions import *
-from utils.transformation_functions import *
-import random
-import pandas as pd
 from datetime import datetime
 from configparser import ConfigParser
+import glob
 
+#lecture du fichier de configuration
+parser = ConfigParser() 
+parser.read('D:/git_repo/data_handling/data_project/src/config_load_to_local.ini')
+
+# Récupération des paramètres [extract_data] 
+path_to_extract = parser.get('extract_data', 'path_to_extract')
+file_mask = parser.get('extract_data', 'file_mask')
+fiel_sep = parser.get('extract_data', 'fiel_sep')
+
+# Récupération des paramètres [transform]
+col_cal_to_add = parser.get('transform', 'col_cal_to_add')
+col_to_concat1 = parser.get('transform', 'col_to_concat1')
+col_to_concat2 = parser.get('transform', 'col_to_concat2')
+sort_column = parser.get('transform', 'sort_column')
+col_to_add = parser.get('transform', 'col_to_add')
+
+
+# Récupération des paramètres [load_data_in_s3]
+path_to_load = parser.get('load_data_into_local_dir', 'path_to_load')
+file_name = parser.get('load_data_into_local_dir', 'file_name')
 
 # Fonction etl d'Extraction, de transformation et de chargement de données.
 def etl(path_to_extract, file_mask, fiel_sep, col_cal_to_add, col_to_concat1, col_to_concat2, sort_column, col_to_add, value_to_add, path_to_load, file_name):
@@ -18,17 +36,10 @@ def etl(path_to_extract, file_mask, fiel_sep, col_cal_to_add, col_to_concat1, co
     #Charegement
     load_data(df1, path_to_load, file_name)
 
-# Appel à la fonction etl
-path_to_extract = "../data/input"
-file_mask = "*.csv"
-fiel_sep = ";" 
-col_cal_to_add = 'nom_prenom', 
-col_to_concat1 = "nom"
-col_to_concat2 = "prenom" 
-sort_column = "age"
-col_to_add = "date_traitement"
-path_to_load = "../data/output/"
-file_name = "client_tous_magasins.csv" 
+
+# Date et heure du traitement 
 now = datetime.now()
 value_to_add = now.strftime("%Y%m%d-%H%M%S")
+
+# Appel à la fonction etl
 etl(path_to_extract, file_mask, fiel_sep, col_cal_to_add, col_to_concat1, col_to_concat2, sort_column, col_to_add, value_to_add, path_to_load, file_name)
